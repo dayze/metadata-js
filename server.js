063ae0d8c1ad4d1a2732fcf11c5cd04d0c71181a
solenedorey <solene.dorey@outlook.com>
2017-11-07 23:37:55 +0100

    File upload : first version (renaming the file and moving it into the "uploads" directory)

--- server.js
+++ server.js
@@ -5,6 +5,8 @@ const express = require("express")
 const fileExtension = require("file-extension")
 const path = require("path")
 const fs = require("fs")
+const multer = require("multer")
+const formidable = require('formidable')
 
 /* PERSONNAL MODULE
    ========================================================================== */
@@ -19,59 +21,81 @@ app.use("/public", express.static(__dirname + "/public"))
 app.set("views", __dirname + "/views")
 app.set("view engine", "twig")
 app.set("twig options", {
-  strict_variables: false
+    strict_variables: false
 })
 
 app.get("/", (req, res) => { //add uniqid + generer fichier json image
-  new Promise((resolve) => {
-    fs.readdir(`${imagesRoot}`, (err, files) => {
-      let exifsInfo = []
-      for (let file of files) {
-        let extension = fileExtension(file)
-        if (extension === "json") {
-          let exifInfo = exif.getExifFromFile(`${imagesRoot}/${file}`)
-          exifInfo.data.id = path.basename(file, `.${extension}`)
-          exifsInfo.push(exifInfo)
-        }
-      }
-      resolve(exifsInfo)
+    new Promise((resolve) => {
+        fs.readdir(`${imagesRoot}`, (err, files) => {
+            let exifsInfo = []
+            for (let file of files) {
+                let extension = fileExtension(file)
+                if (extension === "json") {
+                    let exifInfo = exif.getExifFromFile(`${imagesRoot}/${file}`)
+                    exifInfo.data.id = path.basename(file, `.${extension}`)
+                    exifsInfo.push(exifInfo)
+                }
+            }
+            resolve(exifsInfo)
+        })
+    }).then((exifsInfo) => {
+        res.render("index", {exifsInfo})
     })
-  }).then((exifsInfo) => {
-    res.render("index", {exifsInfo})
-  })
 })
 
 app.get("/detail/:id", (req, res) => {
-  new Promise((resolve) => {
-    fs.readdir(`${imagesRoot}`, (err, files) => {
-      let exifInfo = {}
-      for (let file of files) {
-        let extension = fileExtension(file)
-        if (extension === "json") {
-          if (path.basename(file, `.${extension}`) === req.params.id) {
-            exifInfo = exif.getExifFromFile(`${imagesRoot}/${file}`)
-            exifInfo.data.jsonPath = `${imagesRoot}/${file}`
-            break
-          }
-        }
-      }
-      resolve(exifInfo)
+    new Promise((resolve) => {
+        fs.readdir(`${imagesRoot}`, (err, files) => {
+            let exifInfo = {}
+            for (let file of files) {
+                let extension = fileExtension(file)
+                if (extension === "json") {
+                    if (path.basename(file, `.${extension}`) === req.params.id) {
+                        exifInfo = exif.getExifFromFile(`${imagesRoot}/${file}`)
+                        exifInfo.data.jsonPath = `${imagesRoot}/${file}`
+                        break
+                    }
+                }
+            }
+            resolve(exifInfo)
+        })
+    }).then((exifInfo) => {
+        Object.keys(exifInfo).length !== 0 ? res.render("detail", {exifInfo}) : res.render("detail", {exifInfo: {error: true}})
+    }).catch((e) => {
+        console.log(e)
     })
-  }).then((exifInfo) => {
-    Object.keys(exifInfo).length !== 0 ? res.render("detail", {exifInfo}) : res.render("detail", {exifInfo: {error: true}})
-  }).catch((e) => {
-    console.log(e)
-  })
 })
 
 app.get("/upload", (req, res) => {
-  res.render("upload", {})
+    res.render("upload")
+})
+
+let storage = multer.diskStorage({
+    destination: function(req, file, callback) {
+        callback(null, "uploads/");
+    },
+    filename: function(req, file, callback) {
+        callback(null, file.fieldname + "_" + Date.now() + "_" + file.originalname);
+    }
+});
+
+let upload = multer({
+    storage: storage
+}).single('file-to-upload')
+
+app.post("/update-metadata", (req, res) => {
+    upload(req, res, function(err) {
+        if (err) {
+            return res.end("Something went wrong!")
+        }
+        res.render("update_metadata")
+    });
 })
 
 app.get("/page-technique", (req, res) => {
-  res.render("technique_page", {})
+    res.render("technique_page", {})
 })
 
 app.listen(3000, () => {
-  console.log(`Starting on 3000`)
+    console.log(`Starting on 3000`)
 })
