615fd5a3c1eee635f2bdf68f06772ec80d26b436
Jeremy Habit <jeremy.habit@gmail.com>
2017-12-10 11:49:26 +0100

    complements + responsive flickr

--- public/js/flickrModal.js
+++ public/js/flickrModal.js
@@ -1,12 +1,15 @@
-window.addEventListener("load", function () {
-  let modal = document.querySelector("#modalContainerFlickr")
-  let activators = document.querySelectorAll(".modalFlickrActivator")
+window.addEventListener('load', function () {
+  let body = document.body;
+  let modal = document.querySelector('#modalContainerFlickr')
+  let activators = document.querySelectorAll('.modalFlickrActivator')
   for (let i = 0; i < activators.length; i++) {
-    activators[i].addEventListener("click", function (e) {
-      if (modal.style.display === "block") {
-        modal.style.display = "none"
+    activators[i].addEventListener('click', function (e) {
+      if (modal.style.display === 'block') {
+        body.style.overflowY = 'auto'
+        modal.style.display = 'none'
       } else {
-        modal.style.display = "block"
+        body.style.overflowY = 'hidden'
+        modal.style.display = 'block'
       }
     })
   }
